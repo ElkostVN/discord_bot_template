0bb131ede901951e2a658fa3d42f8c9327be480a
ElkostD <konstantin.i@devstark.com>
2022-12-07 04:27:41 +0300

    refactor: Added deconstuctorization for InteractionComponent class

--- src/lib/classes/interaction-component.js
+++ src/lib/classes/interaction-component.js
@@ -1,8 +1,8 @@
 import { BaseComponent } from '#lib/classes/base-component.js'
 
 export class InteractionComponent extends BaseComponent {
-	constructor (options) {
+	constructor ({ customId, ...options }) {
 		super(options)
-		this.customId = options.customId
+		this.customId = customId
 	}
 }
